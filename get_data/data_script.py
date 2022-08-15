edac8f297173f7b980f095845e3b8b20aedcb3e9
curiousdweller <82277883+curiousdweller@users.noreply.github.com>
2022-08-15 11:41:15 +0100

    Update data_script.py

--- get_data/data_script.py
+++ get_data/data_script.py
@@ -12,7 +12,7 @@ def main():
     get_evm_data("snowtrace.io", "avax")
     get_sol_data()
     
-
+# No need to handle the case where there are no tx on 01/01/2021 since all chains were active since then
 def get_evm_data(link, name):
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"}
     r = requests.get(f"https://{link}/chart/tx?output=csv", headers=headers)
